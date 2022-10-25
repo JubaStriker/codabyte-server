a46028392ee2fe62c8dc70ba85e4ba99ece4a24a
JubaStriker <jubastriker@gmail.com>
2022-10-26 00:13:44 +0600

    running

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const courses = require('./data/Course.json')
+const details = require('./data/DetailesCourse.json');
+const { request } = require('express');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('API running')
+})
+
+app.get('/courses', (req, res) => {
+    request.send(courses)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
